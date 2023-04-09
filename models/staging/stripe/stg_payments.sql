0994636f45398f35aade360e77c6b523fac3f3b0
Clariza Mayo <mayo@energyhub.net>
2023-04-09 07:37:58 +0000

    jinda, models and macros

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,12 +1,18 @@
 with payments as (
 
     select
+        id as payment_id,
         orderid as order_id,
-        sum(case when status = 'success' then amount end) as amount
+        paymentmethod as payment_method,
+        status,
+        -- amount is stored in cents, convert it to dollars
+        {{ cents_to_dollars('amount') }} as amount,
+        created as created_at
 
     from {{ source('stripe', 'payment') }}
-    group by 1
 
 )
 
 select * from payments
+
+{{ limit_data_in_dev(column_name='created_at', dev_days_of_data=10000) }}
