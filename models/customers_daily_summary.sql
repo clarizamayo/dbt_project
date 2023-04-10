b7a9b0399557b8392280aeac7bc3294a62c45896
Clariza Mayo <mayo@energyhub.net>
2023-04-10 04:34:08 +0000

    packages and updated project

--- models/customers_daily_summary.sql
+++ models/customers_daily_summary.sql
@@ -1,3 +1,4 @@
+with customer_daily_summary as (
 select 
     {{ dbt_utils.generate_surrogate_key(['customer_id', 'order_id']) }} as id,
     customer_id,
@@ -5,3 +6,6 @@ select
     count(*)
 from {{ ref('stg_orders') }}
 group by 1,2,3
+)
+
+select * from customer_daily_summary
